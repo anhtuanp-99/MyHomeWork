91f230c2362b6f2d3417d81ca6d3671e8b8a3783
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-05-18 15:19:30 +0700

    update bt3

--- MD02/Session3/ex3/src/EmployeeSalaryManager.java
+++ MD02/Session3/ex3/src/EmployeeSalaryManager.java
@@ -36,7 +36,7 @@ public class EmployeeSalaryManager {
                             break; // kết thúc và thoát vòng lặp
                         }
 
-                        if(salary < 0 || salary > 500){
+                        if(salary < 0 || salary > 500000000){
                             System.out.println("Lương không hợp lệ từ (0 đến 500 triệu!");
                             continue;
                         }
@@ -44,20 +44,20 @@ public class EmployeeSalaryManager {
                         salaries.add(salary); // thêm lương vào danh sách
 
                         String level;
-                        if(salary < 5){
+                        if(salary < 5000000){
                             level = "Thu nhập thấp";
                         }
-                        else if(salary < 15){
+                        else if(salary < 15000000){
                             level = "Thu nhập trung bình";
                         }
-                        else if(salary < 50){
+                        else if(salary < 50000000){
                             level = "Thu nhập khá";
                         }
                         else {
                             level = "Thu nhập cao";
                         }
 
-                        System.out.println("Lương %.2f: %s%n", salary, level);
+                        System.out.printf("Lương %.2f: %s%n", salary, level);
                     }
 
                     //Thống kê
@@ -80,19 +80,65 @@ public class EmployeeSalaryManager {
                     break;
 
                 case 2:
-
+                      if(salaries.isEmpty()){
+                          System.out.println("Chưa có dữ liệu!");
+                          continue;
+                      }
+                      else{
+                          double total = 0, max = Double.MIN_VALUE, min = Double.MAX_VALUE;
+                          for(double s : salaries){
+                              total += s;
+                              if(s > max) max = s;
+                              if(s < min) min = s;
+                          }
+                          double average = total / salaries.size(); // lương trung bình
+                          System.out.println("=========Thống kê========");
+                          System.out.println("Số nhân viên: " + salaries.size());
+                          System.out.printf("Tổng lương: %.2f%n", total);
+                          System.out.printf("Lương trung bình: %.2f%n", average);
+                          System.out.printf("Lương cao nhất: %.2f%n", max);
+                          System.out.printf("Lương thấp nhất: %.2f%n", min);
+                      }
                     break;
 
-                case 3:
-
+                case 3: // Tính tổng số tiền thưởng cho nhân viên
+                    if(salaries.isEmpty()){
+                        System.out.println("Chưa có dữ liệu.");
+                    }
+                    else {
+                        double totalBonus = 0;
+                        for(double s : salaries){
+                            double bonusRate;
+                            if(s < 5000000){
+                                bonusRate = 0.05;
+                            }
+                            else if(s < 15000000){
+                                bonusRate = 0.1;
+                            }
+                            else if(s < 50000000){
+                                bonusRate = 0.15;
+                            }
+                            else if(s < 100000000){
+                                bonusRate = 0.2;
+                            }
+                            else {
+                                bonusRate = 0.25;
+                            }
+                            totalBonus += s * bonusRate;
+                        }
+                        System.out.printf("Tổng tiền thưởng cho tất cả nhân viên: %,.2f%n", totalBonus);
+                    }
                     break;
 
                 case 4:
-
+                    System.out.println("Kết thúc chương trình.");
+                    System.exit(0);
                     break;
+
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng nhập lại");
             }
 
         }while (true);
-        sc.close();
     }
 }
